e981c950d5251c133c0badbd3fb82ab5d39135a9
Deyve Vieira <deyvedvm@hotmail.com>
2018-07-15 10:03:39 -0300

    Initial commit

---
+++ src/main/java/br/com/befullstack/eiticrud/EitiCrudApplication.java
@@ -0,0 +1,12 @@
+package br.com.befullstack.eiticrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EitiCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EitiCrudApplication.class, args);
+    }
+}
---
+++ src/test/java/br/com/befullstack/eiticrud/EitiCrudApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.befullstack.eiticrud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EitiCrudApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
