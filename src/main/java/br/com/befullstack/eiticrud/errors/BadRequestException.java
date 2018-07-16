ec13d7e6af582d8bf01af37eea5d8772ec8f3bf1
Deyve Vieira <deyvedvm@hotmail.com>
2018-07-15 22:44:43 -0300

    Errors Classes

---
+++ src/main/java/br/com/befullstack/eiticrud/errors/BadRequestException.java
@@ -0,0 +1,12 @@
+package br.com.befullstack.eiticrud.errors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * BadRequest Exception
+ */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException {
+
+}
