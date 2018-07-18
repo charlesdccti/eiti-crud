b1513a3db0fea036ea48a6addc15e7114557af13
Deyve Vieira <deyvedvm@hotmail.com>
2018-07-18 16:06:30 -0300

    Update Test UserController

--- src/test/java/br/com/befullstack/eiticrud/controllers/UserControllerTest.java
+++ src/test/java/br/com/befullstack/eiticrud/controllers/UserControllerTest.java
@@ -1,5 +1,6 @@
 package br.com.befullstack.eiticrud.controllers;
 
+import br.com.befullstack.eiticrud.errors.UserNotFoundException;
 import br.com.befullstack.eiticrud.models.User;
 import br.com.befullstack.eiticrud.services.UserService;
 import org.junit.Before;
@@ -8,17 +9,22 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ActiveProfiles("dev")
 @RunWith(SpringRunner.class)
@@ -27,6 +33,9 @@ public class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
 
     @MockBean
     private UserService userService;
@@ -51,21 +60,75 @@ public class UserControllerTest {
 
         mockUserList.add(mockUser);
 
+        mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
     }
 
     @Test
-    public void shouldInsertUser() throws Exception {
+    public void shouldReturnToHome() throws Exception {
 
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
 
     @Test
     public void shouldReturnUsersList() throws Exception {
 
-        given(userService.findAll()).willReturn(mockUserList);
+        when(userService.findAll())
+                .thenReturn(mockUserList);
 
-        this.mockMvc.perform(get("/users"))
-                .andExpect(status().isOk());
+        mockMvc.perform(get("/users").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("users"))
+                .andExpect(view().name("users/user-list"));
+    }
 
+    @Test
+    public void shouldInsertUser() throws Exception {
 
+        when(userService.saveUser(isA(User.class))).thenReturn(mockUser);
+
+        mockMvc.perform(post("/users").contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/users"));
+    }
+
+    @Test
+    public void shouldEditUser() throws Exception {
+
+        when(userService.findById(1)).thenReturn(mockUser);
+
+        mockMvc.perform(
+                get("users/edit/{id}", mockUser.getId()))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("user"))
+                .andExpect(view().name("users/user-form"));
+    }
+
+    @Test
+    public void shouldDeleteUser() throws Exception {
+
+        when(userService.deleteUser(mockUser.getId())).thenReturn(mockUser);
+
+        mockMvc.perform(get("/users/delete/{id}").param("id", mockUser.getId().toString()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("redirect:/users"));
+
+        verify(userService, times(1)).deleteUser(mockUser.getId());
+        verifyNoMoreInteractions(userService);
+    }
+
+    @Test
+    public void findByIdUserNotFoundsShouldRender404View() throws Exception {
+
+        when(userService.findById(1)).thenThrow(new UserNotFoundException(""));
+
+        mockMvc.perform(get("/todo/{id}", 1L))
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("404"));
+
+        // verify(mockUser, times(1)).findById(1);
+
+        verifyZeroInteractions(userService);
     }
 }
